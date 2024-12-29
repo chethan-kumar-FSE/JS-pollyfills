2aaa6a608a05c51fdb04463fc02a7a02a07a0bad
chethankumar123 <chethananderson4@gmail.com>
2024-12-29 22:22:49 +0530

    throttle-trailing-execution-pollyfills.js

---
+++ throttle-pollyfills.js
@@ -0,0 +1,25 @@
+function throttle(fn, delay) {
+  let lastTime = 0;
+  let timeout;
+  return function (...args) {
+    const currentTime = Date.now();
+    if (currentTime - lastTime > delay) {
+      lastTime = currentTime;
+      fn(...args);
+    } else if (!timeout) {
+      timeout = setTimeout(() => {
+        console.log("exuted");
+        timeout = null;
+        lastTime = Date.now();
+        fn(...args);
+      }, delay - (currentTime - lastTime));
+    }
+  };
+}
+function fn(...args) {
+  console.log("excuting the api call", args);
+}
+const throttledLog = throttle(fn, 2000);
+setTimeout(() => throttledLog("Call 1"), 0); // At T=0ms
+setTimeout(() => throttledLog("Call 2"), 500); // At T=500ms
+setTimeout(() => throttledLog("Call 3"), 1000); // At T=1000ms
